8b30e3bf713c0246bedc1f835559e1c5137487c9
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2025-10-29 15:13:54 -0500

    Add javac command to compile Java files

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
+javac -cp "./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar:" -d ./bin ./src/*.java ./src/animals/*.java
+
 java -cp "./bin:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar:" org.junit.runner.JUnitCore AnimalsTest
